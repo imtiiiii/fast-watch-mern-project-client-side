740b267c64e68231ea2453dd347a150e58f122d1
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-11-10 16:49:41 +0600

    home setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Components/Home/Home/Home';
 function App() {
   return (
     <div className="App">
+      <Router>
 
+        <Switch>
+          {/* Home compo */}
+          <Route path="/" exact>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,13 @@
+// home component
+import { Toolbar, Typography } from '@mui/material';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>hello home</h1>
+        </div>
+    );
+};
+
+export default Home;
