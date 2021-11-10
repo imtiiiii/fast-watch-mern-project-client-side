8058b4c362f504a6ae520d1e330437b327adeae8
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-11-10 17:56:20 +0600

    navigation done

---
+++ src/Components/Header/Header/Header.js
@@ -0,0 +1,13 @@
+// header compo
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Header = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Header;
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,11 +1,13 @@
 // home component
 import { Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import Header from '../../Header/Header/Header';
+import Navigation from '../../Shared/Navigation/Navigation';
 
 const Home = () => {
     return (
         <div>
-            <h1>hello home</h1>
+            <Header></Header>
         </div>
     );
 };
